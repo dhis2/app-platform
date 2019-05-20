6f60764286a1a30d4ea8700b81f4d31e604eabab
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-05-20 13:08:42 +0100

    feat: add data-loading to simple example app

--- examples/simple/src/App.js
+++ examples/simple/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import i18n from './locales'
+import { DataQuery } from '@dhis2/app-runtime'
 
 const style = {
     position: 'absolute',
@@ -14,10 +15,34 @@ const style = {
     fontSize: '1rem',
 }
 
+const FailureMessage = ({ baseUrl }) => (
+    <span
+        style={{ color: 'red', fontSize: 18, margin: 16, textAlign: 'center' }}
+    >
+        {i18n.t('Failed to load!')}
+        <br />
+        <a target="_blank" href={`${baseUrl}`}>
+            {i18n.t('Click here to login')}
+        </a>
+    </span>
+)
 const Component = ({ config }) => (
     <div style={style}>
-        <h1>{i18n.t('HELLO WORLD!')}</h1>
-        <h3>DHIS2 URL: {config.url}</h3>
+        <DataQuery
+            query={{
+                me: {
+                    resource: 'me',
+                },
+            }}
+        >
+            {({ error, loading, data }) => {
+                if (error) return <FailureMessage baseUrl={config.url} />
+                if (loading) return <span style={{ color: 'yellow' }}>...</span>
+                return (
+                    <h1>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
+                )
+            }}
+        </DataQuery>
     </div>
 )
 
