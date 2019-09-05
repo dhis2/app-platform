7275160fc500b841dabfb360606604f8152d8783
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-09-05 20:34:19 +0200

    chore: remove superfluous comments

--- cli/src/lib/bundleApp.js
+++ cli/src/lib/bundleApp.js
@@ -6,22 +6,18 @@ const { reporter } = require('@dhis2/cli-helpers-engine')
 
 module.exports = (inDir, outFile) => {
     return new Promise((resolve, reject) => {
-        // create a file to stream archive data to.
         fs.ensureDirSync(path.dirname(outFile))
         const output = fs.createWriteStream(outFile)
 
         const archive = archiver('zip', {
-            zlib: { level: 9 }, // Sets the compression level.
+            zlib: { level: 9 },
         })
 
-        // listen for all archive data to be written
-        // 'close' event is fired only when a file descriptor is involved
         output.on('close', function() {
             reporter.print(chalk.dim(`Total size: ${archive.pointer()} bytes`))
             resolve()
         })
 
-        // good practice to catch warnings (ie stat failures and other non-blocking errors)
         archive.on('warning', function(err) {
             if (err.code === 'ENOENT') {
                 reporter.warn('[bundler]', err)
@@ -30,12 +26,10 @@ module.exports = (inDir, outFile) => {
             }
         })
 
-        // good practice to catch this error explicitly
         archive.on('error', function(err) {
             reject(err)
         })
 
-        // pipe archive data to the file
         archive.pipe(output)
         archive.directory(inDir, false)
         archive.finalize()
