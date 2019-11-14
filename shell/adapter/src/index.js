ece94247eb3186369ac5750badb47859438036b4
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-11-14 13:14:01 +0100

    fix: use Provider instead of DataProvider (#163)

--- shell/adapter/src/index.js
+++ shell/adapter/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { HeaderBar } from '@dhis2/ui-widgets'
-import { DataProvider } from '@dhis2/app-runtime'
+import { Provider } from '@dhis2/app-runtime'
 import { FatalErrorBoundary } from './FatalErrorBoundary'
 import { AuthBoundary } from './AuthBoundary'
 
 const App = ({ url, apiVersion, appName, children }) => (
     <FatalErrorBoundary>
-        <DataProvider baseUrl={url} apiVersion={apiVersion}>
+        <Provider config={{ baseUrl: url, apiVersion: apiVersion }}>
             <HeaderBar appName={appName} />
             <AuthBoundary url={url}>{children}</AuthBoundary>
-        </DataProvider>
+        </Provider>
     </FatalErrorBoundary>
 )
 
