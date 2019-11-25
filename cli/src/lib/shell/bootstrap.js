3808ccd29c89e61264782b7caaadb44811f158fa
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-11-25 15:38:02 +0100

    fix: cache shell version and overwrite on version mismatch

--- cli/src/lib/shell/bootstrap.js
+++ cli/src/lib/shell/bootstrap.js
@@ -2,12 +2,40 @@ const fs = require('fs-extra')
 const path = require('path')
 const { reporter, chalk, exec } = require('@dhis2/cli-helpers-engine')
 
+const scriptsVersion = require(path.join(__dirname, '../../../package.json'))
+    .version
+
+const getShellVersion = metaFile => {
+    if (fs.existsSync(metaFile)) {
+        try {
+            const meta = require(metaFile)
+            return meta.version
+        } catch (e) {
+            reporter.debug('Failed to load meta file', e)
+            // ignore
+        }
+    }
+    return '0'
+}
+
 const bootstrapShell = async (paths, { shell, force = false } = {}) => {
     const source = shell ? path.resolve(shell) : paths.shellSource,
-        dest = paths.shell
+        dest = paths.shell,
+        metaFile = path.join(paths.d2, 'meta.json')
 
     if (fs.pathExistsSync(dest)) {
-        if (!shell && !force) {
+        const versionMismatch = getShellVersion(metaFile) !== scriptsVersion
+        if (versionMismatch) {
+            reporter.print(
+                chalk.dim(
+                    chalk.yellow(
+                        'Local shell version does not match scripts version'
+                    )
+                )
+            )
+        }
+
+        if (!shell && !force && !versionMismatch) {
             reporter.print(
                 chalk.dim(
                     `A local appShell exists, skipping bootstrap. ${chalk.bold(
@@ -17,7 +45,7 @@ const bootstrapShell = async (paths, { shell, force = false } = {}) => {
             )
             return dest
         }
-        reporter.print(chalk.dim('Removing existing directory'))
+        reporter.print(chalk.dim('Removing existing directory...'))
         await fs.remove(dest)
     }
 
@@ -32,6 +60,10 @@ const bootstrapShell = async (paths, { shell, force = false } = {}) => {
             src.indexOf('node_modules', source.length) === -1 &&
             src.indexOf('.pnp', source.length) === -1 &&
             src.indexOf(paths.shellAppDirname) === -1,
+    })
+
+    await fs.writeJSONSync(metaFile, {
+        version: scriptsVersion,
     })
 
     reporter.print(chalk.dim('Installing dependencies...'))
