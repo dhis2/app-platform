e29b4ae321e8bc3456f153eea2d380c0e962b10f
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-11-26 13:31:11 +0100

    chore: inject eslint-disable comment to generated code

--- cli/config/app.babel.config.js
+++ cli/config/app.babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     plugins: [
+        [
+            require('@comandeer/babel-plugin-banner'),
+            {
+                banner: '/* eslint-disable */',
+            },
+        ],
         // Always build in "production" mode even when styled-jsx runtime may select "development"
         [require('styled-jsx/babel'), { optimizeForSpeed: true }],
     ],
