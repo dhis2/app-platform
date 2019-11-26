baedc90302ac8d47d27f2841cd6c133e2a237e9a
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-11-26 18:07:46 +0100

    feat: add support for class properties

--- cli/config/app.babel.config.js
+++ cli/config/app.babel.config.js
@@ -6,6 +6,9 @@ module.exports = {
                 banner: '/* eslint-disable */',
             },
         ],
+
+        require('@babel/plugin-proposal-class-properties'),
+
         // Always build in "production" mode even when styled-jsx runtime may select "development"
         [require('styled-jsx/babel'), { optimizeForSpeed: true }],
     ],
