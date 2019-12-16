7c51addbfab40543b0066e551fceaaa8d3e45492
Viktor Varland <varl@vlv.io>
2019-12-16 08:33:53 +0100

    chore(deps-dev): update cli-style and hooks

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,14 @@
+const { config } = require('@dhis2/cli-style')
+const husky = require(config.husky)
+
+const tasks = arr => arr.join(' && ')
+
 module.exports = {
     hooks: {
-        'commit-msg': 'd2-style commit check',
-        'pre-commit': 'd2-style validate',
+        ...husky.hooks,
+        'pre-commit': tasks([
+            'd2-style js check --staged',
+            'd2-style text check --staged',
+        ]),
     },
 }
