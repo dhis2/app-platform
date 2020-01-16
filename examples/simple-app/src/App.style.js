aeb43740848504134e6850fd34715e4a57062339
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-01-16 12:07:40 +0100

    fix: control the viewport, always show the headerbar, size app container

--- examples/simple-app/src/App.style.js
+++ examples/simple-app/src/App.style.js
@@ -2,11 +2,9 @@ import css from 'styled-jsx/css'
 
 export default css`
     div {
-        position: absolute;
-        top: 48px;
-        bottom: 0;
-        left: 0;
-        right: 0;
+        width: 100%;
+        height: 100%;
+
         display: flex;
         flex-direction: column;
         align-items: center;
