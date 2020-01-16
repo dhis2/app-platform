3823d8a76cbcbd248cf52289aee98c8300a18bc6
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-01-16 16:01:55 +0100

    fix: control the viewport, always show the headerbar, size app container (#267)
    * fix: control the viewport, always show the headerbar, size app container
    
    * chore: fix codestyle and yarn.lock
    
    * chore: use 100% width and height in generated App.js

--- adapter/src/index.js
+++ adapter/src/index.js
@@ -4,12 +4,19 @@ import { Provider } from '@dhis2/app-runtime'
 import { FatalErrorBoundary } from './FatalErrorBoundary'
 import { AuthBoundary } from './AuthBoundary'
 
+import { styles } from './styles.js'
+
 // eslint-disable-next-line react/prop-types
 const App = ({ url, apiVersion, appName, children }) => (
     <FatalErrorBoundary>
         <Provider config={{ baseUrl: url, apiVersion: apiVersion }}>
-            <HeaderBar appName={appName} />
-            <AuthBoundary url={url}>{children}</AuthBoundary>
+            <div className="app-shell-adapter">
+                <style jsx>{styles}</style>
+                <HeaderBar appName={appName} />
+                <AuthBoundary url={url}>
+                    <div className="app-shell-app">{children}</div>
+                </AuthBoundary>
+            </div>
         </Provider>
     </FatalErrorBoundary>
 )
---
+++ adapter/src/styles.js
@@ -0,0 +1,15 @@
+import css from 'styled-jsx/css'
+
+export const styles = css`
+    div.app-shell-adapter {
+        width: 100vw;
+        height: 100vh;
+
+        display: flex;
+        flex-direction: column;
+    }
+    div.app-shell-app {
+        flex: 1 1 auto;
+        overflow: auto;
+    }
+`
