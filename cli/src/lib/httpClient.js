7d8cefae2ffa4aa3a99c44fd016cc48f43aabb66
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-09-02 13:56:50 +0200

    fix: remove default timeout

--- cli/src/lib/httpClient.js
+++ cli/src/lib/httpClient.js
@@ -4,7 +4,6 @@ module.exports.createClient = ({ baseUrl, auth, ...options }) => {
     return axios.create({
         baseURL: baseUrl,
         auth: auth,
-        timeout: 30000,
         ...options,
     })
 }
