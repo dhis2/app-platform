6fab73a1513088f1587410555b61aa10efafb8c5
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-01-13 14:46:56 +0100

    fix: prevent automatic find-up of eslint, use local config in dev

---
+++ shell/.eslintrc.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const eslintRunningLocally = process.cwd() === __dirname
+
+const delegateEslintConfig = path.resolve(__dirname, '../.eslintrc.js') // This should only exist when in development!  However it will pick up .d2/.eslintrc.s in an app, but that shouldn't exist
+const shouldDelegate =
+    !eslintRunningLocally && fs.existsSync(delegateEslintConfig)
+
+const extendsList = shouldDelegate
+    ? [delegateEslintConfig, 'react-app']
+    : 'react-app'
+module.exports = {
+    ignorePatterns: shouldDelegate ? [] : ['src/D2App/*'],
+    extends: extendsList,
+}
