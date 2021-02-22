2fbc42b185e2c6dd6f37a78c570ff26bdc1c068e
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-02-22 21:10:34 +0100

    feat: add support for generating manifest app_hub_id from d2.config id

--- examples/simple-app/d2.config.js
+++ examples/simple-app/d2.config.js
@@ -1,9 +1,10 @@
 const config = {
+    id: 'ASDF1234', // This must match the AppHub ID to successfully publish
     name: 'simple-app',
     title: 'Simple Example App',
     description: 'This is a simple example application',
 
-    standalone: true, // Don't bake-in a DHIS2 base URL, allow the user to choose
+    // standalone: true, // Don't bake-in a DHIS2 base URL, allow the user to choose
 
     entryPoints: {
         app: './src/App',
