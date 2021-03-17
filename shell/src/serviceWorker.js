b47ca8cc6f64f40899efbd977cf56a0f56195318
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-17 14:41:31 +0000

    fix: catch service worker unregistration rejection

--- shell/src/serviceWorker.js
+++ shell/src/serviceWorker.js
@@ -129,8 +129,15 @@ function checkValidServiceWorker(swUrl, config) {
 
 export function unregister() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then(registration => {
-            registration.unregister()
-        })
+        navigator.serviceWorker.ready
+            .then(registration => {
+                registration.unregister()
+            })
+            .catch(error => {
+                console.error(
+                    'Error during service worker unregistration:',
+                    error
+                )
+            })
     }
 }
