2e3cb8c20b5e4019c6fb2dc8410a2cf80362a407
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-03-26 13:52:33 +0100

    chore(graphql plugin): restructure __test__ folder

--- config/gql-to-app-runtime-query/src/__test__/gqlToAppRuntimeQuery.test.js
+++ config/gql-to-app-runtime-query/src/__test__/gqlToAppRuntimeQuery.test.js
@@ -3,9 +3,13 @@ const path = require('path')
 const babel = require('@babel/core')
 const { gqlToAppRuntimeQuery } = require('../gqlToAppRuntimeQuery')
 
-const codePath = path.join(__dirname, 'code.js')
-const code = fs.readFileSync(codePath, { encoding: 'utf8' })
-const runtimeQueryPath = path.join(__dirname, 'runtimeQuery.js')
+const createTestFilePath = fileName =>
+    path.join(__dirname, 'gqlToAppRuntimeQuery', fileName)
+
+const graphqlQueryPath = createTestFilePath('graphqlQuery.js')
+const runtimeQueryPath = createTestFilePath('runtimeQuery.js')
+
+const graphqlQuery = fs.readFileSync(graphqlQueryPath, { encoding: 'utf8' })
 const runtimeQuery = fs.readFileSync(runtimeQueryPath, { encoding: 'utf8' })
 
 describe('gqlToAppRuntimeQuery', () => {
@@ -14,12 +18,25 @@ describe('gqlToAppRuntimeQuery', () => {
             plugins: [gqlToAppRuntimeQuery],
         })
 
-    it('should replace the graphql query with an app-runtime query', () => {
-        // Vim automatically appends empty lines.
-        // So the file with the expected code has an empty line.
-        // Needs to taken into account with the generated code
-        const actual = `${transform(code).code}\n`
-        const expected = runtimeQuery
-        expect(actual).toBe(expected)
+    describe('query', () => {
+        it('should replace the graphql query with an app-runtime query', () => {
+            // Vim automatically appends empty lines.
+            // So the file with the expected code has an empty line.
+            // Needs to taken into account with the generated code
+            const actual = `${transform(graphqlQuery).code}\n`
+            const expected = runtimeQuery
+            expect(actual).toBe(expected)
+        })
+    })
+
+    describe('mutation', () => {
+        it('should replace the graphql mutation with an app-runtime mutation', () => {
+            // Vim automatically appends empty lines.
+            // So the file with the expected code has an empty line.
+            // Needs to taken into account with the generated code
+            const actual = `${transform(graphqlQuery).code}\n`
+            const expected = runtimeQuery
+            expect(actual).toBe(expected)
+        })
     })
 })
