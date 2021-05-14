cf1bc5a88b2fed5caf8de82abb11e9ff8d84db4f
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-05-14 12:33:24 +0200

    docs(example app): add graphql query to example app

--- examples/simple-app/src/App.js
+++ examples/simple-app/src/App.js
@@ -1,9 +1,17 @@
-import { useDataQuery } from '@dhis2/app-runtime'
+import { gql, useDataQuery } from '@dhis2/app-runtime'
 import moment from 'moment'
 import React from 'react'
 import { Alerter } from './Alerter.js'
 import style from './App.style'
 import i18n from './locales'
+
+const queryId = gql`
+    query {
+        me(resource: "me") {
+            id
+        }
+    }
+`
 
 const query = {
     me: {
@@ -13,21 +21,29 @@ const query = {
 
 const Component = () => {
     const { error, loading, data } = useDataQuery(query)
+    const { error: errorId, loading: loadingId, data: dataId } = useDataQuery(
+        queryId
+    )
+
     return (
         <div>
             <style jsx>{style}</style>
             {error && <span>ERROR</span>}
-            {loading && <span>...</span>}
+            {errorId && <span>ERROR ID</span>}
+            {(loading || loadingId) && <span>...</span>}
             {data && (
                 <>
                     <h1>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
-                    <h3>
+                    <h2>
                         {i18n.t('Have a great {{dayOfTheWeek}}!', {
                             dayOfTheWeek: moment.weekdays(true)[
                                 moment().weekday()
                             ],
                         })}
-                    </h3>
+                    </h2>
+
+                    {dataId && <h3>Id of me: {dataId.me.id}</h3>}
+
                     <Alerter />
                 </>
             )}
