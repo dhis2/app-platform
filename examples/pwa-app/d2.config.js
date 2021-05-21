930259ac7e48354e2ccf6715bbb75cdcf80ad919
Kai Vandivier <kpvandivier@gmail.com>
2021-05-21 15:25:01 +0200

    chore: set up demo config & scripts

--- examples/pwa-app/d2.config.js
+++ examples/pwa-app/d2.config.js
@@ -1,6 +1,16 @@
 const config = {
     type: 'app',
 
+    standalone: true,
+    pwa: {
+        enabled: true,
+        caching: {
+            omitExternalRequests: false,
+            // For the purposes of this demo:
+            patternsToOmit: ['visualizations'],
+        },
+    },
+
     entryPoints: {
         app: './src/App.js',
     },
