fd04c96508fbac72469548455cad02d888a95557
Kai Vandivier <kpvandivier@gmail.com>
2021-06-08 17:31:12 +0200

    refactor: move offline interface and provider to app adapter

--- adapter/src/index.js
+++ adapter/src/index.js
@@ -1,3 +1,5 @@
+import { OfflineProvider } from '@dhis2/app-service-offline'
+import { OfflineInterface } from '@dhis2/sw'
 import { HeaderBar } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -7,17 +9,21 @@ import { FatalErrorBoundary } from './components/FatalErrorBoundary'
 import { ServerVersionProvider } from './components/ServerVersionProvider'
 import { styles } from './styles.js'
 
+const offlineInterface = new OfflineInterface()
+
 const App = ({ url, apiVersion, appName, children }) => (
     <FatalErrorBoundary>
         <ServerVersionProvider url={url} apiVersion={apiVersion}>
-            <div className="app-shell-adapter">
-                <style jsx>{styles}</style>
-                <HeaderBar appName={appName} />
-                <AuthBoundary url={url}>
-                    <div className="app-shell-app">{children}</div>
-                </AuthBoundary>
-                <Alerts />
-            </div>
+            <OfflineProvider offlineInterface={offlineInterface}>
+                <div className="app-shell-adapter">
+                    <style jsx>{styles}</style>
+                    <HeaderBar appName={appName} />
+                    <AuthBoundary url={url}>
+                        <div className="app-shell-app">{children}</div>
+                    </AuthBoundary>
+                    <Alerts />
+                </div>
+            </OfflineProvider>
         </ServerVersionProvider>
     </FatalErrorBoundary>
 )
--- examples/pwa-app/src/App.js
+++ examples/pwa-app/src/App.js
@@ -1,18 +1,12 @@
-import { OfflineProvider } from '@dhis2/app-service-offline'
-import { OfflineInterface } from '@dhis2/sw'
 import React from 'react'
 import classes from './App.module.css'
 import SectionWrapper from './components/SectionWrapper'
 import './locales'
 
-const offlineInterface = new OfflineInterface()
-
 const MyApp = () => (
-    <OfflineProvider offlineInterface={offlineInterface}>
-        <div className={classes.container}>
-            <SectionWrapper id={'section-id-01'} />
-        </div>
-    </OfflineProvider>
+    <div className={classes.container}>
+        <SectionWrapper id={'section-id-01'} />
+    </div>
 )
 
 export default MyApp
