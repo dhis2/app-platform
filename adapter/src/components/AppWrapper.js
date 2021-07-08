65c776642353a5c9396a71a1b8b1e3422d16d7d9
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-07-08 15:11:11 +0200

    fix: never render HeaderBar without runtime provider (#587)
    * fix: never render HeaderBar without runtime provider
    
    * style: formatting
    
    * fix: log locale initialization in useEffect, add emoji to log
    
    * fix: short-circuit the system info fetch if no url provided
    
    * style: sort propTypes

---
+++ adapter/src/components/AppWrapper.js
@@ -0,0 +1,29 @@
+import { HeaderBar } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useCurrentUserLocale } from '../utils/useLocale.js'
+import { Alerts } from './Alerts.js'
+import { LoadingMask } from './LoadingMask.js'
+import { styles } from './styles/AppWrapper.style.js'
+
+export const AppWrapper = ({ appName, children }) => {
+    const { loading } = useCurrentUserLocale()
+
+    if (loading) {
+        return <LoadingMask />
+    }
+
+    return (
+        <div className="app-shell-adapter">
+            <style jsx>{styles}</style>
+            <HeaderBar appName={appName} />
+            <div className="app-shell-app">{children}</div>
+            <Alerts />
+        </div>
+    )
+}
+
+AppWrapper.propTypes = {
+    appName: PropTypes.string.isRequired,
+    children: PropTypes.node,
+}
