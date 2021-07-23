82010d594567e12e8c5da64ed32f397c7339fc87
Kai Vandivier <kpvandivier@gmail.com>
2021-07-23 15:35:55 +0200

    chore: remove locales comment

--- examples/pwa-app/src/App.js
+++ examples/pwa-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import classes from './App.module.css'
 import SectionWrapper from './components/SectionWrapper'
-// import './locales'
 
 const MyApp = () => (
     <div className={classes.container}>
