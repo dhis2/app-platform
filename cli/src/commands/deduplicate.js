248024494542fe1cb40646c52492adbeee3f7ab9
Médi-Rémi Hashim <mediremi@gmail.com>
2021-10-04 10:04:36 +0100

    feat(cli): add 'deduplicate' command

---
+++ cli/src/commands/deduplicate.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const { reporter, chalk } = require('@dhis2/cli-helpers-engine')
+const makePaths = require('../lib/paths')
+const { listDuplicates, fixDuplicates } = require('../lib/yarnDeduplicate')
+
+const handler = async ({ cwd }) => {
+    const paths = makePaths(cwd)
+
+    if (paths.yarnLock === null) {
+        reporter.error('Could not find yarn.lock')
+        process.exit(1)
+    }
+
+    const yarnLock = fs.readFileSync(paths.yarnLock, 'utf8')
+    const deduped = fixDuplicates(yarnLock)
+    fs.writeFileSync(paths.yarnLock, deduped)
+
+    const duplicates = listDuplicates(deduped)
+    if (duplicates.size > 0) {
+        reporter.error('Failed to deduplicate the following packages:')
+        for (const [name, versions] of duplicates) {
+            reporter.error(
+                ` * ${chalk.bold(name)} (found versions ${versions.join(', ')})`
+            )
+        }
+        process.exit(1)
+    }
+}
+
+const command = {
+    command: 'deduplicate',
+    desc: 'Deduplicate dependencies found in yarn.lock',
+    handler,
+}
+
+module.exports = command
