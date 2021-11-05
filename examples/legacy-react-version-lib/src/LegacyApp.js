31532a9551ab4586a5522ac931dfeaa9f4fe917f
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-11-05 09:02:51 +0100

    chore: use no-hoist for multiple react versions

--- examples/legacy-react-version-lib/src/LegacyApp.js
+++ examples/legacy-react-version-lib/src/LegacyApp.js
@@ -1,7 +1,9 @@
 import React from 'react'
+// import { FlatButton,  } from 'material-ui';
 
 export const LegacyApp = () => (
     <div>
         <h1>I am leg(end|acy)!</h1>
+        {/* <FlatButton>Hello World</FlatButton>; */}
     </div>
 )
