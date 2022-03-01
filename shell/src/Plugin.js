cec63391562ace95cfc1ce94019fd395fd0f8aa8
Médi-Rémi Hashim <mediremi@gmail.com>
2022-03-01 14:40:57 +0000

    feat(cli): add webpack config for JS and CSS

--- shell/src/Plugin.js
+++ shell/src/Plugin.js
@@ -1,1 +1,14 @@
-// placeholder
+import { CssReset } from '@dhis2/ui'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Plugin from './Plugin'
+import 'typeface-roboto'
+import './index.css'
+
+ReactDOM.render(
+    <>
+        <CssReset />
+        <Plugin />
+    </>,
+    document.getElementById('dhis2-app-root')
+)
