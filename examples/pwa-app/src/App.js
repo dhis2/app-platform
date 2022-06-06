ebc74ef84aa0c10fb6eb25b54dc9b360e41aceb6
Kai Vandivier <kpvandivier@gmail.com>
2022-06-06 15:19:36 +0200

    chore(pwa-app): add request tester

--- examples/pwa-app/src/App.js
+++ examples/pwa-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import classes from './App.module.css'
+import RequestTester from './components/RequestTester'
 import SectionWrapper from './components/SectionWrapper'
 
 const MyApp = () => (
     <div className={classes.container}>
+        <RequestTester />
         <SectionWrapper id={'section-id-01'} />
     </div>
 )
---
+++ examples/pwa-app/src/components/RequestTester.js
@@ -0,0 +1,38 @@
+import { useDataEngine } from '@dhis2/app-runtime'
+import { useDhis2ConnectionStatus } from '@dhis2/app-service-offline'
+import { Button } from '@dhis2/ui'
+import React from 'react'
+
+const query = {
+    me: {
+        resource: 'me',
+        params: {
+            fields: ['id', 'name'],
+        },
+    },
+}
+
+export default function RequestTester() {
+    const engine = useDataEngine()
+    const { isConnectedToDhis2 } = useDhis2ConnectionStatus()
+
+    const internalRequest = () => {
+        console.log('internal request')
+        engine.query(query)
+    }
+    const externalRequest = () => {
+        console.log('external request')
+        fetch('https://random.dog/woof.json')
+    }
+
+    return (
+        <div>
+            <p>
+                Request Tester.{' '}
+                {isConnectedToDhis2 ? 'Connected' : 'NOT CONNECTED'}
+            </p>
+            <Button onClick={internalRequest}>Internal Request</Button>
+            <Button onClick={externalRequest}>External Request</Button>
+        </div>
+    )
+}
