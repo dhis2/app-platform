29ddd6c96ae39f094ab98e505cb398c316a32cd3
ismay <7355199+ismay@users.noreply.github.com>
2022-06-21 09:15:54 +0200

    refactor: move extends and ignorepatterns

--- shell/.eslintrc.js
+++ shell/.eslintrc.js
@@ -8,12 +8,9 @@ const isRunningHere = process.cwd() === __dirname
 const hasRootConfig = fs.existsSync(rootConfigPath)
 const isDevelopment = !isRunningHere && hasRootConfig
 
-// Use local config for developing this library, react-app preset for linting app code
-const extendsList = isDevelopment ? rootConfigPath : 'react-app'
-// Ignore app code that the dev has no control over
-const ignorePatterns = isDevelopment ? [] : ['src/D2App/*']
-
 module.exports = {
-    ignorePatterns,
-    extends: extendsList,
+    // Ignore app code that the dev has no control over
+    ignorePatterns: isDevelopment ? [] : ['src/D2App/*'],
+    // Use local config for developing this library, react-app preset for linting app code
+    extends: isDevelopment ? rootConfigPath : 'react-app',
 }
