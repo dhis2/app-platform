1eea12c825145480d4fbf2807d3078ef608f2325
Kai Vandivier <kpvandivier@gmail.com>
2022-08-23 17:26:16 +0200

    fix: plugin placeholder

--- shell/src/Plugin.js
+++ shell/src/Plugin.js
@@ -1,14 +1,1 @@
-import { CssReset } from '@dhis2/ui'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Plugin from './Plugin'
-import 'typeface-roboto'
-import './index.css'
-
-ReactDOM.render(
-    <>
-        <CssReset />
-        <Plugin />
-    </>,
-    document.getElementById('dhis2-app-root')
-)
+// placeholder
