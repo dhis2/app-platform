ff5aa6bd1da8705811ec62f7e47537ba7c7f0822
Kai Vandivier <kpvandivier@gmail.com>
2022-08-23 17:38:27 +0200

    fix: casing

--- shell/src/plugin.index.js
+++ shell/src/plugin.index.js
@@ -1,7 +1,7 @@
 import { CssReset } from '@dhis2/ui'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Plugin from './plugin.js'
+import Plugin from './Plugin.js'
 import 'typeface-roboto'
 import './index.css'
 
