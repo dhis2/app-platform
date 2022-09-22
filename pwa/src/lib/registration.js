b9333b12efe4d01317e986bea0cdb71191641509
Kai Vandivier <kpvandivier@gmail.com>
2022-09-22 20:43:23 +0200

    fix(pwa): registration URL on development

--- pwa/src/lib/registration.js
+++ pwa/src/lib/registration.js
@@ -133,7 +133,7 @@ export function register(config) {
         window.addEventListener('load', () => {
             // By compiling the dev SW to the 'public' dir, this URL works in
             // both dev and production modes
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+            const swUrl = new URL('service-worker.js', publicUrl)
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
