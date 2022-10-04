5e462453324465df1003834e291eccb67b281c7b
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-10-05 01:19:52 +0200

    feat: handle session expiration

--- adapter/src/utils/api.js
+++ adapter/src/utils/api.js
@@ -5,6 +5,7 @@ const request = (url, options) => {
         fetch(url, {
             ...options,
             credentials: 'include',
+            redirect: 'manual',
             headers: {
                 'X-Requested-With': 'XMLHttpRequest',
                 Accept: 'application/json',
@@ -42,3 +43,50 @@ export const post = (url, body) =>
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     })
+
+const checkAuthenticationStatus = async (server) => {
+    try {
+        await get(`${server}/api/me?fields=username`)
+        return true
+    } catch {
+        // Note: this also fails if the server is down or gives any error
+        return false
+    }
+}
+
+export const attemptLogin = async ({
+    server,
+    username,
+    password,
+    mfaToken,
+    onSuccess,
+    onFailure,
+}) => {
+    try {
+        const authData = {
+            j_username: username,
+            j_password: password,
+            '2fa': mfaToken ? '2fa' : undefined,
+            '2fa_code': mfaToken || undefined,
+        }
+        const postBody = Object.entries(authData)
+            .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+            .join('&')
+        await post(`${server}/dhis-web-commons-security/login.action`, postBody)
+    } catch (e) {
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(
+                'TODO: This will always error and cancel the request until we get a real login endpoint!'
+            )
+        }
+    }
+
+    const succeeded = await checkAuthenticationStatus(server)
+    if (succeeded) {
+        console.log(`Login successful for ${username} (${server})`)
+        onSuccess()
+    } else {
+        console.log(`Login attempt failed for ${username} (${server})`)
+        onFailure()
+    }
+}
