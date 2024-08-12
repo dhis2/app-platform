4eda4a9287ee9d1abe93c5f82647c3a028939bb3
Kai Vandivier <49666798+KaiVandivier@users.noreply.github.com>
2024-08-12 15:20:41 +0200

    feat(init): set direction: 'auto' and import locales for new apps [LIBS-645] (#867)
    * feat: add direction: 'auto' to new apps' config
    
    * feat: import locales from boilerplate entrypoint

--- cli/config/d2ConfigDefaults.js
+++ cli/config/d2ConfigDefaults.js
@@ -1,3 +1,11 @@
+/**
+ * Note that these values are different from the boilerplate files that
+ * are copied when using `d2-app-scripts init` -- these are used as defaults
+ * when parsing d2 config of existing apps
+ *
+ * The boilerplate files live in /config/init/
+ */
+
 const defaultsApp = {
     type: 'app',
 
