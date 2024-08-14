40c11ceefe8af642fa4a2301f78a757dca1f875e
Kai Vandivier <kpvandivier@gmail.com>
2024-08-14 12:35:27 +0200

    refactor: use babel for AST parsing

--- cli/src/commands/jsx-migration.js
+++ cli/src/commands/jsx-migration.js
@@ -1,37 +1,133 @@
 const fs = require('fs/promises')
 const path = require('path')
+const babel = require('@babel/core')
 const { reporter /* chalk */ } = require('@dhis2/cli-helpers-engine')
 const fg = require('fast-glob')
 
-// Looks for JSX syntax; avoids comments
-// Known false positives: in strings or multiline comments (see regex101)
-// https://regex101.com/r/YDkml7/6
-const jsxRegex = /^(?![ \t]*(?:\/\/|\/?\*+)).*[^a-zA-Z](<\/?[a-zA-Z]+[^>]*>)/gim
+const babelParseOptions = {
+    // could just use jsx syntax parser, but this is already a dep of CLI
+    presets: ['@babel/preset-react'],
+    // just need syntax parser here
+    plugins: ['@babel/plugin-syntax-flow'],
+}
 
-// Looks for relative import statements
-// https://regex101.com/r/xsHZdQ/3
-const importRegex = /(import.*|from) ['"](\..*)['"]/gim
+const isJsxInFile = async (filepath) => {
+    const code = await fs.readFile(filepath, { encoding: 'utf8' })
+    try {
+        const ast = await babel.parseAsync(code, babelParseOptions)
 
-const handler = async ({ dontAddExtensionsToImportsWithoutOne }) => {
-    const globMatches = await fg.glob('src/**/*.js')
+        let isJsx = false
+        babel.traverse(ast, {
+            // Triggers for any JSX-type node (JSXElement, JSXAttribute, etc)
+            JSX: (path) => {
+                isJsx = true
+                path.stop() // done here; stop traversing
+            },
+        })
 
-    // 1. Search each file for JSX syntax
+        return isJsx
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+const renameFile = async (filepath) => {
+    const newPath = filepath.concat('x') // Add 'x' to the end to make it 'jsx'
+    reporter.debug(`Renaming ${filepath} to ${newPath}`)
+    await fs.rename(filepath, newPath)
+}
+
+const updateImports = async ({
+    filepath,
+    renamedFiles,
+    skipUpdatingImportsWithoutExtension,
+}) => {
+    const code = await fs.readFile(filepath, { encoding: 'utf8' })
+    let newCode = code
+    try {
+        const ast = await babel.parseAsync(code, babelParseOptions)
+
+        let contentUpdated = false
+        babel.traverse(ast, {
+            ImportDeclaration: (astPath) => {
+                const importSource = astPath.node.source.value
+                if (!importSource.startsWith('.')) {
+                    return // not a relative import
+                }
+
+                // This is a little weird for files with an extension other
+                // than .js, but it's okay since they won't need updating
+                const importSourceHasExtension = importSource.endsWith('.js')
+                if (
+                    skipUpdatingImportsWithoutExtension &&
+                    !importSourceHasExtension
+                ) {
+                    return
+                }
+
+                // We'll need an extension to match with the renamed files Set
+                const importSourceWithExtension = importSourceHasExtension
+                    ? importSource
+                    : importSource + '.js'
+                // get the full path of the imported file from the cwd
+                const importPathFromCwd = path.join(
+                    filepath,
+                    '..',
+                    importSourceWithExtension
+                )
+                const isRenamed = renamedFiles.has(importPathFromCwd)
+
+                // Since generating code from babel doesn't respect formatting,
+                // update imports with just string replacement
+                if (isRenamed) {
+                    // updating & replacing the raw value, which includes quotes,
+                    // ends up being more precise and avoids side effects
+                    const rawImportSource = astPath.node.source.extra.raw
+                    const newRawImportSource = rawImportSource.replace(
+                        importSource,
+                        importSourceWithExtension + 'x'
+                    )
+                    newCode = newCode.replace(
+                        rawImportSource,
+                        newRawImportSource
+                    )
+                    contentUpdated = true
+                    console.log({ newRawImportSource, contentUpdated })
+                }
+            },
+        })
+
+        if (contentUpdated) {
+            await fs.writeFile(filepath, newCode)
+            return true
+        }
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+// todo: test filepaths
+// const filepath = '../capture-app/src/core_modules/capture-core/rules/getApplicableRuleEffects.js'
+// const filepath = '../capture-app/src/core_modules/capture-core/components/D2Form/D2Form.component.js'
+// const filepath = 'examples/pwa-app/src/App.js'
+
+// todo: hmm -- now that we're parsing ASTs, it may be best to use a map of
+// { path: ast } ... or would that be too much memory?
+
+const handler = async ({ skipUpdatingImportsWithoutExtension }) => {
+    // 1. Search each JS file for JSX syntax
     // If found, 1) add to Set and 2) rename file (add 'x' to end)
+    const globMatches = await fg.glob('src/**/*.js')
+    // todo: progress bar: map index / globMatches.length
+    reporter.info(`Searching for JSX in ${globMatches.length} files...`)
     const renamedFiles = new Set()
     await Promise.all(
         globMatches.map(async (matchPath) => {
-            const contents = await fs.readFile(matchPath, {
-                encoding: 'utf8',
-            })
-            if (!contents) {
-                return
-            }
-
-            if (contents.match(jsxRegex)) {
-                const newPath = matchPath.concat('x') // Add 'x' to the end to make it 'jsx'
-                reporter.debug(`Renaming ${matchPath} to ${newPath}`)
-
-                await fs.rename(matchPath, newPath)
+            const jsxIsInFile = await isJsxInFile(matchPath)
+            if (jsxIsInFile) {
+                await renameFile(matchPath, renamedFiles)
                 renamedFiles.add(matchPath)
             }
         })
@@ -45,69 +141,27 @@ const handler = async ({ dontAddExtensionsToImportsWithoutOne }) => {
     // (Note: Files without extension aren't edited; Vite and TS
     // handle them, so it's up to eslint rules)
     const globMatches2 = await fg.glob('src/**/*.(js|jsx)')
+    // todo: progress bar: map index / globMatches.length
+    reporter.info(`Scanning ${globMatches2.length} files to update imports...`)
     let fileUpdatedCount = 0
     await Promise.all(
         globMatches2.map(async (matchPath) => {
-            const fileContent = await fs.readFile(matchPath, {
-                encoding: 'utf8',
+            const importsAreUpdated = await updateImports({
+                filepath: matchPath,
+                renamedFiles,
+                skipUpdatingImportsWithoutExtension,
             })
-
-            let newFileContent = fileContent
-            let contentUpdated = false
-            const importMatches = Array.from(fileContent.matchAll(importRegex))
-            importMatches.forEach((match) => {
-                // match[2] is the second capturing group, the import path
-                const importPath = match[2]
-                // This is a little weird for files with an extension other
-                // than .js, but it's okay since they won't need renaming
-                const importPathHasExtension = importPath.endsWith('.js')
-                if (
-                    !importPathHasExtension &&
-                    dontAddExtensionsToImportsWithoutOne
-                ) {
-                    return
-                }
-
-                // We'll need an extension to match with the renamed files Set
-                const importPathWithExtension = importPathHasExtension
-                    ? importPath
-                    : importPath + '.js'
-                // get the full path of the imported file from the repo root
-                const importPathFromRoot = path.join(
-                    matchPath,
-                    '..',
-                    importPathWithExtension
-                )
-                const isRenamed = renamedFiles.has(importPathFromRoot)
-
-                if (isRenamed) {
-                    // replacing the whole matched string ends up being more
-                    // precise than just the import text and avoids side effects
-                    const newMatchContent = match[0].replace(
-                        importPath,
-                        importPathWithExtension + 'x'
-                    )
-                    newFileContent = newFileContent.replace(
-                        match[0],
-                        newMatchContent
-                    )
-                    contentUpdated = true
-                }
-            })
-
-            if (contentUpdated) {
-                await fs.writeFile(matchPath, newFileContent)
-                fileUpdatedCount += 1
+            if (importsAreUpdated) {
+                fileUpdatedCount++
             }
         })
     )
-    if (fileUpdatedCount > 0) {
-        reporter.info(`Updated imports in ${fileUpdatedCount} file(s)`)
-    }
+    reporter.info(`Updated imports in ${fileUpdatedCount} file(s)`)
 
     // 3. Update d2.config.js
     // Read d2 config as JS for easier access to entryPoint strings
     const d2ConfigPath = path.join(process.cwd(), './d2.config.js')
+    // todo: verify file exists before requiring
     const { entryPoints } = require(d2ConfigPath)
     const d2ConfigContents = await fs.readFile(d2ConfigPath, {
         encoding: 'utf8',
@@ -136,7 +190,7 @@ const command = {
     desc: 'Renames .js files to .jsx -- also handles file imports and d2.config.js',
     builder: {
         // todo: update parameters
-        dontAddExtensionsToImportsWithoutOne: {
+        skipUpdatingImportsWithoutExtension: {
             description:
                 "Normally, this script will update `import './App'` to `import './App.jsx'`. Use this flag to skip adding the extension in this case. Imports that already end with .js will still be updated to .jsx",
             type: 'boolean',
