c8ed60c22093e88e52e70add700509193dd1e3fe
Kai Vandivier <kpvandivier@gmail.com>
2024-08-14 17:13:42 +0200

    fix: improve d2 config handling

--- cli/src/commands/jsx-migration.js
+++ cli/src/commands/jsx-migration.js
@@ -38,6 +38,45 @@ const renameFile = async (filepath) => {
     await fs.rename(filepath, newPath)
 }
 
+/**
+ * For JS imports, this will handle imports either with or without a .js
+ * extension, such that the result ends with .jsx if the target file has been
+ * renamed.
+ * Files without extension are updated by default since some linting rules give
+ * `import/no-unresolved` errors after switching to JSX if imports don't use
+ * an extension
+ *
+ * If `skipUpdatingImportsWithoutExtension` is set, imports without an extension
+ * will be left as-is
+ */
+const updateImportSource = ({
+    filepath,
+    importSource,
+    renamedFiles,
+    skipUpdatingImportsWithoutExtension,
+}) => {
+    // This is a little weird for files with an extension other
+    // than .js, but it's okay since they won't need updating
+    const importSourceHasExtension = importSource.endsWith('.js')
+    if (skipUpdatingImportsWithoutExtension && !importSourceHasExtension) {
+        return
+    }
+
+    // We'll need an extension to match with the renamed files Set
+    const importSourceWithExtension = importSourceHasExtension
+        ? importSource
+        : importSource + '.js'
+    // get the full path of the imported file from the cwd
+    const importPathFromCwd = path.join(
+        filepath,
+        '..',
+        importSourceWithExtension
+    )
+
+    const isRenamed = renamedFiles.has(importPathFromCwd)
+    return isRenamed ? importSourceWithExtension + 'x' : importSource
+}
+
 const updateImports = async ({
     filepath,
     renamedFiles,
@@ -58,35 +97,19 @@ const updateImports = async ({
                     return // not a relative import
                 }
 
-                // This is a little weird for files with an extension other
-                // than .js, but it's okay since they won't need updating
-                const importSourceHasExtension = importSource.endsWith('.js')
-                if (
-                    skipUpdatingImportsWithoutExtension &&
-                    !importSourceHasExtension
-                ) {
-                    return
-                }
-
-                // We'll need an extension to match with the renamed files Set
-                const importSourceWithExtension = importSourceHasExtension
-                    ? importSource
-                    : importSource + '.js'
-                // get the full path of the imported file from the cwd
-                const importPathFromCwd = path.join(
+                const newImportSource = updateImportSource({
                     filepath,
-                    '..',
-                    importSourceWithExtension
-                )
-                const isRenamed = renamedFiles.has(importPathFromCwd)
+                    importSource,
+                    renamedFiles,
+                    skipUpdatingImportsWithoutExtension,
+                })
 
                 // Since generating code from babel doesn't respect formatting,
                 // update imports with just string replacement
-                if (isRenamed) {
+                if (newImportSource !== importSource) {
                     // updating & replacing the raw value, which includes quotes,
                     // ends up being more precise and avoids side effects
                     const rawImportSource = astPath.node.source.extra.raw
-                    const newImportSource = importSourceWithExtension + 'x'
                     const newRawImportSource = rawImportSource.replace(
                         importSource,
                         newImportSource
@@ -118,14 +141,10 @@ const updateImports = async ({
 // const filepath = '../capture-app/src/core_modules/capture-core/components/D2Form/D2Form.component.js'
 // const filepath = 'examples/pwa-app/src/App.js'
 
-// todo: hmm -- now that we're parsing ASTs, it may be best to use a map of
-// { path: ast } ... or would that be too much memory?
-
 const handler = async ({ skipUpdatingImportsWithoutExtension }) => {
     // 1. Search each JS file for JSX syntax
     // If found, 1) add to Set and 2) rename file (add 'x' to end)
     const globMatches = await fg.glob('src/**/*.js')
-    // todo: progress bar: map index / globMatches.length
     reporter.info(`Searching for JSX in ${globMatches.length} files...`)
     const renamedFiles = new Set()
     await Promise.all(
@@ -146,7 +165,6 @@ const handler = async ({ skipUpdatingImportsWithoutExtension }) => {
     // (Note: Files without extension aren't edited; Vite and TS
     // handle them, so it's up to eslint rules)
     const globMatches2 = await fg.glob('src/**/*.(js|jsx)')
-    // todo: progress bar: map index / globMatches.length
     reporter.info(`Scanning ${globMatches2.length} files to update imports...`)
     let fileUpdatedCount = 0
     await Promise.all(
@@ -164,29 +182,49 @@ const handler = async ({ skipUpdatingImportsWithoutExtension }) => {
     reporter.info(`Updated imports in ${fileUpdatedCount} file(s)`)
 
     // 3. Update d2.config.js
-    // Read d2 config as JS for easier access to entryPoint strings
-    const d2ConfigPath = path.join(process.cwd(), './d2.config.js')
-    // todo: verify file exists before requiring
-    const { entryPoints } = require(d2ConfigPath)
+    const d2ConfigPath = path.join(process.cwd(), 'd2.config.js')
+    reporter.info('Checking d2.config.js for entry points to update...')
+    reporter.debug(`d2 config path: ${d2ConfigPath}`)
+
+    // Read d2 config as JS for easy access to entryPoint strings
+    let entryPoints
+    try {
+        const d2Config = require(d2ConfigPath)
+        entryPoints = d2Config.entryPoints
+    } catch (err) {
+        reporter.info(`Did not find d2.config.js at ${d2ConfigPath}; finishing`)
+        return
+    }
+
     const d2ConfigContents = await fs.readFile(d2ConfigPath, {
         encoding: 'utf8',
     })
     let newD2ConfigContents = d2ConfigContents
     let configContentUpdated = false
     Object.values(entryPoints).forEach((entryPoint) => {
-        // entryPoint is formatted as './src/...' -- drop first 2 chars
-        // to match the glob format above
-        if (renamedFiles.has(entryPoint.substring(2))) {
+        const newEntryPointSource = updateImportSource({
+            filepath: 'd2.config.js',
+            importSource: entryPoint,
+            renamedFiles,
+            skipUpdatingImportsWithoutExtension,
+        })
+        if (newEntryPointSource !== entryPoint) {
             newD2ConfigContents = newD2ConfigContents.replace(
                 entryPoint,
-                entryPoint + 'x'
+                newEntryPointSource
             )
             configContentUpdated = true
+            reporter.debug(
+                `Updating entry point ${entryPoint} => ${newEntryPointSource}`
+            )
         }
     })
+
     if (configContentUpdated) {
         await fs.writeFile(d2ConfigPath, newD2ConfigContents)
-        reporter.info('Updated d2.config.js entrypoints')
+        reporter.info('Updated d2.config.js entry points')
+    } else {
+        reporter.info('No entry points updated')
     }
 }
 
