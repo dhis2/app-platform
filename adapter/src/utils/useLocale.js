9a1f1359486d08718f449bbb22a4dbcd03cd0d23
Kai Vandivier <kpvandivier@gmail.com>
2026-03-02 20:49:28 +0100

    fix: check system settings before fetching translations

--- adapter/src/utils/useLocale.js
+++ adapter/src/utils/useLocale.js
@@ -1,4 +1,4 @@
-import { useDataQuery } from '@dhis2/app-runtime'
+import { useConfig, useDataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import { useState, useEffect, useMemo } from 'react'
 import { useCustomTranslations } from './customTranslations.js'
@@ -12,8 +12,9 @@ import {
 const useLocale = ({
     userSettings,
     configDirection,
-    customTranslationsEnabled,
+    customTranslationsInfo,
 }) => {
+    const { appUrlSlug } = useConfig()
     const getCustomTranslations = useCustomTranslations()
     const [result, setResult] = useState({
         locale: undefined,
@@ -26,13 +27,30 @@ const useLocale = ({
         }
 
         const locale = parseLocale(userSettings)
+        const dhis2Locale = userSettings.keyUiLocale
+
+        let shouldFetchCustomTranslations = false
+        if (customTranslationsInfo) {
+            try {
+                // In the `custom-translations/controller` data store key,
+                // is there configured an object { [appUrlSlug]: dhis2Locale[] }
+                // that includes the current dhis2Locale in the array?
+                shouldFetchCustomTranslations =
+                    customTranslationsInfo[appUrlSlug]?.includes(dhis2Locale)
+                if (!shouldFetchCustomTranslations) {
+                    console.debug(
+                        'Custom translations not found in controller for this app and locale.'
+                    )
+                }
+            } catch (err) {
+                console.error('Error parsing custom translation controller')
+                console.error(err)
+            }
+        }
 
         // Asynchronous - check datastore for custom translations if enabled
-        const customTranslationsPromise = customTranslationsEnabled
-            ? getCustomTranslations({
-                  locale,
-                  dhis2Locale: userSettings.keyUiLocale,
-              })
+        const customTranslationsPromise = shouldFetchCustomTranslations
+            ? getCustomTranslations({ locale, dhis2Locale })
             : Promise.resolve()
 
         // Synchronous -- will resolve before state is set and the child app is rendered
@@ -51,28 +69,52 @@ const useLocale = ({
         userSettings,
         configDirection,
         getCustomTranslations,
-        customTranslationsEnabled,
+        customTranslationsInfo,
+        appUrlSlug,
     ])
 
     return result
 }
 
-const settingsQuery = {
+// NOTE: This info would be nice to get from the Global Shell
+const USER_SETTINGS_QUERY = {
     userSettings: {
         resource: 'userSettings',
     },
-    customTranslationsEnabled: {
-        // TODO: Use new setting
-        resource: 'systemSettings', // /keyCustomTranslationsEnabled
+}
+// For use in v43+
+const CUSTOM_TRANSLATIONS_QUERY = {
+    setting: {
+        resource: 'systemSettings/keyCustomTranslationsEnabled',
+    },
+    // The values in this key are expected to be in the format
+    // { [appUrlSlug]: dhis2LocaleCode[] }
+    info: {
+        resource: 'dataStore/custom-translations/controller',
     },
 }
-// note: userSettings.keyUiLocale is expected to be in the Java format,
+// note: userSettings.keyUiLocale is expected to be in the DHIS2 locale format,
 // e.g. 'ar', 'ar_IQ', 'uz_UZ_Cyrl', etc.
 export const useCurrentUserLocale = (configDirection) => {
-    const { loading, error, data } = useDataQuery(settingsQuery)
+    const { serverVersion } = useConfig()
+    const customTranslationsAvailable = serverVersion.minor >= 43
+
+    const { loading, error, data } = useDataQuery(USER_SETTINGS_QUERY)
+    const customTranslationsQuery = useDataQuery(CUSTOM_TRANSLATIONS_QUERY, {
+        // Below v43, don't run this query
+        lazy: !customTranslationsAvailable,
+        // If there's an error, it could be because the datastore isn't set up
+        onError: () => console.log('Custom translations not available.'),
+    })
+
+    const customTranslationsInfo =
+        customTranslationsAvailable &&
+        customTranslationsQuery.data?.setting.keyCustomTranslationsEnabled &&
+        customTranslationsQuery.data?.info
+
     const { locale, direction } = useLocale({
         userSettings: data && data.userSettings,
-        customTranslationsEnabled: data && data.customTranslationsEnabled,
+        customTranslationsInfo,
         configDirection,
     })
 
@@ -81,7 +123,11 @@ export const useCurrentUserLocale = (configDirection) => {
         throw new Error('Failed to fetch user locale: ' + error)
     }
 
-    return { loading: loading || !locale, locale, direction }
+    return {
+        loading: loading || customTranslationsQuery.loading || !locale,
+        locale,
+        direction,
+    }
 }
 
 const loginConfigQuery = {
